6b4e99a633d4261667b321e33bc5b0ce63b29cb6
Tag-E <e.taggi@fz-juelich.de>
2025-04-09 14:43:07 +0200

    wrote a simple script to read the the relevant datasets so that after executing the script the related fast access datasets are available

---
+++ moments_pdf/data_reading_script.py
@@ -0,0 +1,62 @@
+#simple script to load the relevant datasets
+
+#library imports
+import os
+from moments_toolkit import moments_toolkit
+
+#paths for the coarse lattice
+p3fold = os.environ['mount_point_path'] + "48c48/binned_1012_hmz370_BMW_extended/3PointCorrelation/"
+p2fold = os.environ['mount_point_path'] + "48c48/binned_1012_hmz370_BMW_extended/2PointCorrelation/"
+
+# coarse lattice - P = 0
+opAnalyzer1 = moments_toolkit(p3fold, p2fold,
+                            skip3p=False, skipop=False,
+                            verbose=True,
+                            fast_data_folder = "fast_data_extended_p0_q0",
+                            operator_folder= "operator_database",
+                            momentum='PX0_PY0_PZ0',
+                            insertion_momentum = 'qx0_qy0_qz0',
+                            tag_2p='hspectrum',
+                            max_n=2 #max_n=3
+                            )
+
+# coarse lattice - Px = -2
+opAnalyzer2 = moments_toolkit(p3fold, p2fold,
+                            skip3p=False, skipop=False,
+                            verbose=True,
+                            fast_data_folder = "fast_data_extended_px-2_q0",
+                            operator_folder= "operator_database",
+                            momentum='PX-2_PY0_PZ0',
+                            insertion_momentum = 'qx0_qy0_qz0',
+                            tag_2p='hspectrum',
+                            max_n=2 #max_n=3
+                            )
+
+
+#paths for the fine lattice
+p3fold = os.environ['mount_point_path'] + "64c64/3pt_binned_h5/"
+p2fold = os.environ['mount_point_path'] + "64c64/2pt_binned_h5/"
+
+# fine lattice - P = 0
+opAnalyzer1 = moments_toolkit(p3fold, p2fold,
+                            skip3p=False, skipop=False,
+                            verbose=True,
+                            fast_data_folder = "fast_data_extended_fine_p0_q0",
+                            operator_folder= "operator_database",
+                            momentum='PX0_PY0_PZ0',
+                            insertion_momentum = 'qx0_qy0_qz0',
+                            tag_2p='hspectrum',
+                            max_n=2 #max_n=3
+                            )
+
+# fine lattice - Px = -2
+opAnalyzer2 = moments_toolkit(p3fold, p2fold,
+                            skip3p=False, skipop=False,
+                            verbose=True,
+                            fast_data_folder = "fast_data_extended_fine_px-2_q0",
+                            operator_folder= "operator_database",
+                            momentum='PX-2_PY0_PZ0',
+                            insertion_momentum = 'qx0_qy0_qz0',
+                            tag_2p='hspectrum',
+                            max_n=2 #max_n=3
+                            )
