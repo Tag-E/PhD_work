b5843d055497e8641c367dc771eef24620fb1e5e
Tag-E <e.taggi@fz-juelich.de>
2025-05-07 12:49:08 +0200

    updated the data reading script: - added the class instance needed to properly load the new dataset suited for 2 derivatives operator analysis - explicited the choice of the smearing parameter in the moments toolkit class instances - different class instances have now different names

--- moments_pdf/data_reading_script.py
+++ moments_pdf/data_reading_script.py
@@ -9,28 +9,30 @@ p3fold = os.environ['mount_point_path'] + "48c48/binned_1012_hmz370_BMW_extended
 p2fold = os.environ['mount_point_path'] + "48c48/binned_1012_hmz370_BMW_extended/2PointCorrelation/"
 
 # coarse lattice - P = 0
-opAnalyzer1 = moments_toolkit(p3fold, p2fold,
-                            skip3p=False, skipop=False,
-                            verbose=True,
-                            fast_data_folder = "fast_data_extended_p0_q0",
-                            operator_folder= "operator_database",
-                            momentum='PX0_PY0_PZ0',
-                            insertion_momentum = 'qx0_qy0_qz0',
-                            tag_2p='hspectrum',
-                            max_n=2 #max_n=3
-                            )
+opAnalyzer_coarse_P0 = moments_toolkit(p3fold, p2fold,
+                                        skip3p=False, skipop=False,
+                                        verbose=True,
+                                        fast_data_folder = "fast_data_extended_p0_q0",
+                                        operator_folder= "operator_database",
+                                        momentum='PX0_PY0_PZ0',
+                                        insertion_momentum = 'qx0_qy0_qz0',
+                                        smearing_index=0,
+                                        tag_2p='hspectrum',
+                                        max_n=2 #max_n=3
+                                        )
 
 # coarse lattice - Px = -2
-opAnalyzer2 = moments_toolkit(p3fold, p2fold,
-                            skip3p=False, skipop=False,
-                            verbose=True,
-                            fast_data_folder = "fast_data_extended_px-2_q0",
-                            operator_folder= "operator_database",
-                            momentum='PX-2_PY0_PZ0',
-                            insertion_momentum = 'qx0_qy0_qz0',
-                            tag_2p='hspectrum',
-                            max_n=2 #max_n=3
-                            )
+opAnalyzer_coarse_Px = moments_toolkit(p3fold, p2fold,
+                                        skip3p=False, skipop=False,
+                                        verbose=True,
+                                        fast_data_folder = "fast_data_extended_px-2_q0",
+                                        operator_folder= "operator_database",
+                                        momentum='PX-2_PY0_PZ0',
+                                        insertion_momentum = 'qx0_qy0_qz0',
+                                        smearing_index=0,
+                                        tag_2p='hspectrum',
+                                        max_n=2 #max_n=3
+                                        )
 
 
 #paths for the fine lattice
@@ -38,25 +40,44 @@ p3fold = os.environ['mount_point_path'] + "64c64/3pt_binned_h5/"
 p2fold = os.environ['mount_point_path'] + "64c64/2pt_binned_h5/"
 
 # fine lattice - P = 0
-opAnalyzer1 = moments_toolkit(p3fold, p2fold,
-                            skip3p=False, skipop=False,
-                            verbose=True,
-                            fast_data_folder = "fast_data_extended_fine_p0_q0",
-                            operator_folder= "operator_database",
-                            momentum='PX0_PY0_PZ0',
-                            insertion_momentum = 'qx0_qy0_qz0',
-                            tag_2p='hspectrum',
-                            max_n=2 #max_n=3
-                            )
+opAnalyzer_fine_P0 = moments_toolkit(p3fold, p2fold,
+                                        skip3p=False, skipop=False,
+                                        verbose=True,
+                                        fast_data_folder = "fast_data_extended_fine_p0_q0",
+                                        operator_folder= "operator_database",
+                                        momentum='PX0_PY0_PZ0',
+                                        insertion_momentum = 'qx0_qy0_qz0',
+                                        smearing_index=0,
+                                        tag_2p='hspectrum',
+                                        max_n=2 #max_n=3
+                                        )
 
 # fine lattice - Px = -1
-opAnalyzer2 = moments_toolkit(p3fold, p2fold,
-                            skip3p=False, skipop=False,
-                            verbose=True,
-                            fast_data_folder = "fast_data_extended_fine_px-1_q0",
-                            operator_folder= "operator_database",
-                            momentum='PX-1_PY0_PZ0',
-                            insertion_momentum = 'qx0_qy0_qz0',
-                            tag_2p='hspectrum',
-                            max_n=2 #max_n=3
-                            )
+opAnalyzer_fine_Px = moments_toolkit(p3fold, p2fold,
+                                        skip3p=False, skipop=False,
+                                        verbose=True,
+                                        fast_data_folder = "fast_data_extended_fine_px-1_q0",
+                                        operator_folder= "operator_database",
+                                        momentum='PX-1_PY0_PZ0',
+                                        insertion_momentum = 'qx0_qy0_qz0',
+                                        smearing_index=0,
+                                        tag_2p='hspectrum',
+                                        max_n=2 #max_n=3
+                                        )
+
+#paths for the coarse lattice needed for 2der operators
+p3fold = os.environ['mount_point_path_newdataset'] + "48c48/binned20250430_hmz370_BMW_3.31_48c48_ml-0.09933_mh-0.04_connected_himom//3PointCorrelation/"
+p2fold = os.environ['mount_point_path_newdataset'] + "48c48/binned20250430_hmz370_BMW_3.31_48c48_ml-0.09933_mh-0.04_connected_himom/2PointCorrelation/"
+
+# coarse lattice - P = -2
+opAnalyzer_coarse_2DER = moments_toolkit(p3fold, p2fold,
+                                            skip3p=False, skipop=False,
+                                            verbose=True,
+                                            fast_data_folder = "fast_data_2DERdataset_p2",
+                                            operator_folder= "operator_database",
+                                            momentum='PX2_PY2_PZ2',
+                                            insertion_momentum = 'qx0_qy0_qz0',
+                                            smearing_index=1,
+                                            tag_2p='hspectrum',
+                                            max_n=3
+                                            )
